32055c66b8bc88b3a5269f4b3f7e64ca096c8ae5
Heloisa Cabral <heloisacabralco@gmail.com>
2023-11-06 21:25:39 -0300

    config software architecture and routes

--- app.js
+++ app.js
@@ -1,2 +1,22 @@
-const express = require("express")
-const app = express()
+//script de backend javascript
+
+const express = require("express");
+const path = require("path");
+const routes = require("./routes/routes");
+
+
+
+const app = express();
+
+const port = 3000;
+
+
+app.set("view engine", "ejs")
+
+app.use(express.static(path.join(__dirname, "public")))
+app.use(routes)
+
+
+app.listen(port, () => {
+  console.log(`Serviço rodando em http://localhost:${port}`);
+});
